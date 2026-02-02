92dcd43a4bef05b51fed20b8fe822c1d5da9a928
patricia5788 <patriciacheng0708@gmail.com>
2026-02-03 00:04:10 +0800

    admin_dasboard.php

--- admin_dasboard.php
+++ admin_dasboard.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+include "db.php";
+
+if (!isset($_SESSION['userID']) || $_SESSION['userRole'] != 'Hospital') {
+    header("Location: login.php");
+    exit();
+}
+
+$notifications = include "processNotifications.php";
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
